47a7802a0099671f7736fee8a383600a649cb844
Daniel Baulig <me@danielbaulig.de>
2023-05-13 12:06:56 -0700

    Add read API sensors for each line

--- custom_components/vestaboard/__init__.py
+++ custom_components/vestaboard/__init__.py
@@ -1,7 +1,13 @@
 """ Vestaboard Integration """
+from datetime import timedelta
 import requests
 import logging
 import json
+import async_timeout
+from homeassistant.helpers.update_coordinator import (
+    DataUpdateCoordinator,
+    UpdateFailed,
+)
 
 _LOGGER=logging.getLogger(__name__)
 
@@ -10,6 +16,7 @@ from .const import DOMAIN, CONF_LOCAL_API_HOST, CONF_LOCAL_API_KEY
 ATTR_LINES="lines"
 
 character_map = {
+    '~': 0,
     ' ': 0,
     'A': 1,
     'B': 2,
@@ -74,57 +81,77 @@ character_map = {
     '\xc5': 67,
     '\xc6': 68,
     '\xc7': 69,
-    '~': 0
 }
 
-async def async_setup_entry(hass, config):
-    host = config.data[CONF_LOCAL_API_HOST]
-    local_api_key = config.data[CONF_LOCAL_API_KEY] 
+inverted_character_map = {v:k for k,v in character_map.items()}
 
-    uri = f"http://{host}:7000/local-api/message"
-    headers = {
-        'X-Vestaboard-Local-Api-Key': local_api_key
-    }
+class Vestaboard:
+    def __init__(self, host, local_api_key):
+        self.host = host
+        self.local_api_key = local_api_key
 
-    async def post(call):
-        nonlocal uri
-        nonlocal headers
-        nonlocal host
+    @property
+    def auth_headers(self):
+        return {
+            'X-Vestaboard-Local-Api-Key': self.local_api_key
+        }
 
-        lines = call.data.get(ATTR_LINES)
+    @property
+    def local_api_uri(self):
+        return f"http://{self.host}:7000/local-api/message"
 
-        lines_array = []
+    def read(self):
+        uri = self.local_api_uri
 
-        for i in range(6):
-            line = '' if i >= len(lines) else (lines[i] or '')
-            normalized_line = '{:<22}'.format(line[:22].upper())
+        try:
+            _LOGGER.debug('Read being sent to %s', uri)
+            response = requests.get(uri, headers=self.auth_headers)
+        except ConnectionError:
+            _LOGGER.error('Failed to connect to %s', self.host)
+            return None
+        except requests.Timeout:
+            _LOGGER.error('Connection attempt timed out connecting to %s', self.host)
+            return None
+        except requests.exceptions.RequestException as err:
+            _LOGGER.error('An unknown request exception occured connecting to %s', self.host)
+            return None
+        except:
+            _LOGGER.error('An unknown exception occured connecting to %s', self.host)
+            return None
 
-            lines_array.append([])
+        if response.status_code < 200 or response.status_code > 299:
+            _LOGGER.error('Read failed with error code %d and message %s', response.status_code, response.text)
+            return None
 
-            for char in normalized_line:
-                lines_array[i].append(
-                    character_map.get(char if char in character_map else '?')
-                )
-        lines_json = json.dumps(lines_array)
-        
+        return [ ''.join([inverted_character_map[code] for code in line]) for line in response.json()['message'] ]
+
+    def post(self, lines):
+        uri = self.local_api_uri
+
+        lines += [''] * (6 - len(lines))
+        normalized_lines = ['{:<22}'.format(line[:22].upper()) for line in lines]
+        encoded_lines = [[character_map.get(char, 60) for char in line] for line in normalized_lines]
+
+        lines_json = json.dumps(encoded_lines)
+
         try:
             _LOGGER.debug('Request being sent to %s with payload %s', uri, lines_json)
-            response = await hass.async_add_executor_job(lambda: requests.post(
+            response = requests.post(
                 uri,
                 data=lines_json,
-                headers=headers,
-            ))
+                headers=self.auth_headers,
+            )
         except ConnectionError:
-            _LOGGER.error('Failed to connect to %s', host)
+            _LOGGER.error('Failed to connect to %s', self.host)
             return False
         except requests.Timeout:
-            _LOGGER.error('Connection attempt timed out connecting to %s', host)
+            _LOGGER.error('Connection attempt timed out connecting to %s', self.host)
             return False
         except requests.exceptions.RequestException as err:
-            _LOGGER.error('An unknown request exception occured connecting to %s', host)
+            _LOGGER.error('An unknown request exception occured connecting to %s', self.host)
             return False
         except:
-            _LOGGER.error('An unknown exception occured connecting to %s', host)
+            _LOGGER.error('An unknown exception occured connecting to %s', self.host)
             return False
 
         if response.status_code < 200 or response.status_code > 299:
@@ -132,6 +159,53 @@ async def async_setup_entry(hass, config):
             return False
 
         return True
+
+
+class VestaboardCoordinator(DataUpdateCoordinator):
+    def __init__(self, hass, vestaboard):
+        super().__init__(
+            hass, _LOGGER, name="Vestaboard",
+            update_interval=timedelta(seconds=30)
+        )
+        self.vestaboard = vestaboard
+
+    async def _async_update_data(self):
+        async with async_timeout.timeout(10):
+            data = await self.hass.async_add_executor_job(lambda: self.vestaboard.read())
+            if data is None:
+                raise UpdateFailed("Couldn't update vestaboard lines sensor.")
+
+            return data
+
+
+async def async_setup_entry(hass, config):
+    host = config.data[CONF_LOCAL_API_HOST]
+    local_api_key = config.data[CONF_LOCAL_API_KEY]
+
+    vestaboard = Vestaboard(host, local_api_key)
+    coordinator = VestaboardCoordinator(hass, vestaboard)
+
+    if DOMAIN not in hass.data:
+        hass.data[DOMAIN] = {}
+    hass.data[DOMAIN][config.entry_id] = {
+        'coordinator': coordinator,
+    }
+
+    await coordinator.async_config_entry_first_refresh()
+
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(
+            config,
+            "sensor"
+        )
+    )
+
+    async def post(call):
+        lines = call.data.get(ATTR_LINES)
+        result = await hass.async_add_executor_job(lambda: vestaboard.post(lines))
+        # Update line sensors after an update
+        config.async_create_task(hass, coordinator.async_request_refresh())
+        return result
 
     hass.services.async_register(DOMAIN, "post", post)
 
---
+++ custom_components/vestaboard/sensor.py
@@ -0,0 +1,32 @@
+from homeassistant.core import callback
+from homeassistant.components.sensor import SensorEntity
+from homeassistant.helpers.update_coordinator import (
+    CoordinatorEntity
+)
+
+import json
+
+from .const import DOMAIN
+
+
+class VestaboardLineEntity(CoordinatorEntity, SensorEntity):
+    _attr_has_entity_name = True
+
+    def __init__(self, coordinator, line):
+        super().__init__(coordinator)
+        self.line = line
+
+    @property
+    def name(self):
+        return f"Vestaboard Line {self.line}"
+
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        self._attr_native_value = self.coordinator.data[self.line]
+        self.async_write_ha_state()
+
+
+async def async_setup_entry(hass, config, async_add_entities):
+    async_add_entities(
+        [VestaboardLineEntity(hass.data[DOMAIN][config.entry_id]['coordinator'], line) for line in range(6)]
+    )
